91848b487614bc9d740a3ae580b177e425516a0a
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2021-09-05 19:30:45 +0200

    Django connection to MySQL fixed

--- App/App/settings.py
+++ App/App/settings.py
@@ -99,8 +99,12 @@ CELERY = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database',
+        'USER': 'user',
+        'PASSWORD': 'password',       
+        'HOST': 'database', # <-- docker host name for db
+        'PORT': '3306', # <-- docker port for db
     }
 }
 
