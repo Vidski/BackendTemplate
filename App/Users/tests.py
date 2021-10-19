345125d2d64bf054505466c07343dedcaeeafe84
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2021-10-19 03:50:45 +0200

    Tests refactored

--- App/Users/tests.py
+++ App/Users/tests.py
@@ -7,23 +7,25 @@ from rest_framework.test import APIClient
 
 from Users.models import User
 
+ENDPOINT = '/api/v1/users'
+
 
 class UsersAbstractUtils(TestCase):
 
     def setUp(self):
         self._clean()
         self.admin_user = self._create_user(is_admin=True, **{
-            'email': 'adminuser@appname.me',
+            'email': 'adminuser@notalone.me',
             'phone_number': '+34123456789'})
         self.normal_user = self._create_user(**{
-            'email': 'normaluser@appname.me',
+            'email': 'normaluser@notalone.me',
             'phone_number': '+032923093'})
         self.client = APIClient()
 
     def _create_user(self, is_admin=False, **kwargs):
         first_name = kwargs.get('name', 'Name')
         last_name = kwargs.get('last_name', 'Last Name')
-        email = kwargs.get('email', 'user@appname.me')
+        email = kwargs.get('email', 'user@notalone.me')
         password = kwargs.get('password', 'password')
         is_admin = kwargs.get('is_admin', is_admin)
         phone_number = kwargs.get('phone_number', '+1234567890')
@@ -90,64 +92,64 @@ class UserTests(UsersAbstractUtils):
 
     def test_list_users(self):
         # Test that an unauthenthicate user can't list users data
-        response = self.client.get(f'/api/v1/users/')
+        response = self.client.get(f'{ENDPOINT}/', format='json')
         self.assertEqual(response.status_code, 401)
 
         # Test that the petition with admin user is valid and returns the 2 users from setup
         self.client.force_authenticate(user=self.admin_user)
-        response = self.client.get('/api/v1/users/')
+        response = self.client.get(f'{ENDPOINT}/', format='json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2) 
 
         # Test that the petition with normal user is denied
         self.client.force_authenticate(user=self.normal_user)
-        response = self.client.get(f'/api/v1/users/')
+        response = self.client.get(f'{ENDPOINT}/', format='json')
         self.assertEqual(response.status_code, 403)
 
         # Test a petition with normal normal user verified is denied
         self.normal_user.is_verified = True
         self.normal_user.save()
-        response = self.client.get(f'/api/v1/users/')
+        response = self.client.get(f'{ENDPOINT}/', format='json')
         self.assertEqual(response.status_code, 403)
 
     def test_get_user(self):
         # Test that an unauthenthicate user can't get users data
-        response = self.client.get(f'/api/v1/users/{self.normal_user.id}/')
+        response = self.client.get(f'{ENDPOINT}/{self.normal_user.id}/', format='json')
         self.assertEqual(response.status_code, 401)
 
         # Test a petition with normal user not verified to its user is denied
         self.client.force_authenticate(user=self.normal_user)
-        response = self.client.get(f'/api/v1/users/{self.normal_user.id}/')
+        response = self.client.get(f'{ENDPOINT}/{self.normal_user.id}/', format='json')
         self.assertEqual(response.status_code, 403)
 
         # Test a petition with normal user verified is granted to its data
         self.normal_user.is_verified = True
         self.normal_user.save()
-        response = self.client.get(f'/api/v1/users/{self.normal_user.id}/')
+        response = self.client.get(f'{ENDPOINT}/{self.normal_user.id}/', format='json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["id"], self.normal_user.id)
         self.assertEqual(response.data["email"], self.normal_user.email)
 
         # Test a petition with normal user verified is denied to other user's data
-        response = self.client.get(f'/api/v1/users/{self.admin_user.id}/')
+        response = self.client.get(f'{ENDPOINT}/{self.admin_user.id}/', format='json')
         self.assertEqual(response.status_code, 403)
 
         # Test a petition with admin user is granted to other user's data
         self.client.force_authenticate(user=self.admin_user)
-        response = self.client.get(f'/api/v1/users/{self.normal_user.id}/')
+        response = self.client.get(f'{ENDPOINT}/{self.normal_user.id}/', format='json')
         self.assertEqual(response.status_code, 200)
 
     def test_sign_up(self):
         # Test that cannot signup with an used email
-        self._create_user(**{'email': 'emailused@appname.me'})
+        self._create_user(**{'email': 'emailused@notalone.me'})
         data = {
             "first_name":"Test",
             "last_name":"Tested",
-            "email":"emailused@appname.me",
+            "email":"emailused@notalone.me",
             "password":"password",
             "password_confirmation":"password"
         }
-        response = self.client.post(f'/api/v1/users/signup/', data)
+        response = self.client.post(f'{ENDPOINT}/signup/', data, format='json')
         message_one = 'email'
         message_two = 'This field must be unique'
         self.assertEqual(response.status_code, 400)
@@ -159,11 +161,11 @@ class UserTests(UsersAbstractUtils):
         data = {
             "first_name":"Test",
             "last_name":"Tested",
-            "email":"unusedemail@appname.me",
+            "email":"unusedemail@notalone.me",
             "password":"password",
             "password_confirmation":"password"
         }
-        response = self.client.post(f'/api/v1/users/signup/', data)
+        response = self.client.post(f'{ENDPOINT}/signup/', data, format='json')
         message = 'This password is too common.'
         self.assertEqual(response.status_code, 400)
         self.assertTrue(message in response.data['non_field_errors'][0])
@@ -172,12 +174,12 @@ class UserTests(UsersAbstractUtils):
         data = {
             "first_name":"Test",
             "last_name":"Tested",
-            "email":"unusedemail@appname.me",
+            "email":"unusedemail@notalone.me",
             "password":"strongpassword",
             "password_confirmation":"strongpassword"
         }
         self.assertEqual(User.objects.count(), 3)
-        response = self.client.post(f'/api/v1/users/signup/', data)
+        response = self.client.post(f'{ENDPOINT}/signup/', data, format='json')
         self.assertEqual(User.objects.count(), 4)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data['first_name'], data['first_name'])
@@ -192,7 +194,7 @@ class UserTests(UsersAbstractUtils):
         data = {
             "first_name":"Test",
             "last_name":"Tested",
-            "email":"unusedemail2@appname.me",
+            "email":"unusedemail2@notalone.me",
             "password":"strongpassword",
             "password_confirmation":"strongpassword",
             "phone_number": "+03999999999",
@@ -201,7 +203,7 @@ class UserTests(UsersAbstractUtils):
             "is_premium": True
         }
         self.assertEqual(User.objects.count(), 4)
-        response = self.client.post(f'/api/v1/users/signup/', data)
+        response = self.client.post(f'{ENDPOINT}/signup/', data, format='json')
         self.assertEqual(User.objects.count(), 5)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data['first_name'], data['first_name'])
@@ -217,29 +219,29 @@ class UserTests(UsersAbstractUtils):
         data = {
             "first_name":"Test edited",
             "last_name":"Tested Edit",
-            "email":"emailedited@appname.me",
+            "email":"edituser2@notalone.me",
             "phone_number": "+32987654321",
             "old_password": "password",
             "password":"NewPassword95"
         }
         # Test that an unauthenthicate user can't update users data
-        response = self.client.put(f'/api/v1/users/{self.normal_user.id}/', data)
+        response = self.client.put(f'{ENDPOINT}/{self.normal_user.id}/', data, format='json')
         self.assertEqual(response.status_code, 401)
-        self.client.force_authenticate(user=self.normal_user)
 
         # Test that an unverified user can't update its data
-        response = self.client.put(f'/api/v1/users/{self.normal_user.id}/', data)
+        self.client.force_authenticate(user=self.normal_user)
+        response = self.client.put(f'{ENDPOINT}/{self.normal_user.id}/', data, format='json')
         self.assertEqual(response.status_code, 403)
         self.normal_user.is_verified = True
         self.normal_user.save()
 
         # Test that a verified user can't update other's data
-        response = self.client.put(f'/api/v1/users/{self.admin_user.id}/', data)
+        response = self.client.put(f'{ENDPOINT}/{self.admin_user.id}/', data, format='json')
         self.assertEqual(response.status_code, 403)
         self.assertNotEqual(self.normal_user.email, data["email"])
 
         # Test that a verified user can update its data
-        response = self.client.put(f'/api/v1/users/{self.normal_user.id}/', data)
+        response = self.client.put(f'{ENDPOINT}/{self.normal_user.id}/', data, format='json')
         self.assertEqual(response.status_code, 202)
         self.normal_user = User.objects.get(id=self.normal_user.id)
         self.assertEqual(self.normal_user.email, data["email"])
@@ -250,14 +252,14 @@ class UserTests(UsersAbstractUtils):
         self.client.force_authenticate(user=self.normal_user)
 
         # Test that an user verified can't update its email to one already used
-        self._create_user(**{'email': 'emailused@appname.me'})
+        self._create_user(**{'email': 'emailused@notalone.me'})
         data = {
             "first_name":"Test",
             "last_name":"Tested",
-            "email":"emailused@appname.me",
+            "email":"emailused@notalone.me",
             "password":"password"
         }
-        response = self.client.put(f'/api/v1/users/{self.normal_user.id}/', data)
+        response = self.client.put(f'{ENDPOINT}/{self.normal_user.id}/', data, format='json')
         self.assertEqual(response.status_code, 400)
         self.assertTrue("Email is taked" in response.data)
 
@@ -266,24 +268,24 @@ class UserTests(UsersAbstractUtils):
         data = {
             "first_name":"Test",
             "last_name":"Tested",
-            "email":"edituser3@appname.me",
+            "email":"edituser3@notalone.me",
             "password":"password",
             "phone_number": "+03999999999"
         }
-        response = self.client.put(f'/api/v1/users/{self.normal_user.id}/', data)
+        response = self.client.put(f'{ENDPOINT}/{self.normal_user.id}/', data, format='json')
         self.assertEqual(response.status_code, 400)
         self.assertTrue("Phone number is taked" in response.data)
-        
+
         # Test a user can't update its password with a wrong old password
         data = {
             "first_name":"Test",
             "last_name":"Tested",
-            "email":"finalemail@appname.me",
+            "email":"finalemail@notalone.me",
             "phone_number": "+32987654321",
             "old_password": "NewPassword95 wrong",
             "password":"This is a password"
         }
-        response = self.client.put(f'/api/v1/users/{self.normal_user.id}/', data)
+        response = self.client.put(f'{ENDPOINT}/{self.normal_user.id}/', data, format='json')
         message = "Wrong password"
         self.assertEqual(response.status_code, 400)
         self.assertTrue(message in response.data)
@@ -292,11 +294,11 @@ class UserTests(UsersAbstractUtils):
         data = {
             "first_name":"Test",
             "last_name":"Tested",
-            "email":"finalemail@appname.me",
+            "email":"finalemail@notalone.me",
             "phone_number": "+32987654321",
             "password":"This is a password"
         }
-        response = self.client.put(f'/api/v1/users/{self.normal_user.id}/', data)
+        response = self.client.put(f'{ENDPOINT}/{self.normal_user.id}/', data, format='json')
         message = 'Old password is required to set a new one'
         self.assertEqual(response.status_code, 400)
         self.assertTrue(message in response.data)
@@ -305,7 +307,7 @@ class UserTests(UsersAbstractUtils):
         data = {
             "first_name":"Test",
             "last_name":"Tested",
-            "email":"emailemail@appname.me",
+            "email":"emailemail@notalone.me",
             "phone_number": "+32987654321",
             "old_password":"NewPassword95",
             "password":"This is a password",
@@ -313,7 +315,7 @@ class UserTests(UsersAbstractUtils):
             "is_admin": True,
             "is_premium": True
         }
-        response = self.client.put(f'/api/v1/users/{self.normal_user.id}/', data)
+        response = self.client.put(f'{ENDPOINT}/{self.normal_user.id}/', data, format='json')
         self.assertEqual(response.status_code, 202)
         self.normal_user = User.objects.get(id=self.normal_user.id)
         self.assertEqual(self.normal_user.email, data["email"])
@@ -331,68 +333,68 @@ class UserTests(UsersAbstractUtils):
             "old_password":"This is a password",
             "password":"Password 123456789"
         }
-        response = self.client.put(f'/api/v1/users/{self.normal_user.id}/', data)
+        response = self.client.put(f'{ENDPOINT}/{self.normal_user.id}/', data, format='json')
         self.assertEqual(response.status_code, 202)
         self.normal_user = User.objects.get(id=self.normal_user.id)
         self.assertTrue(self.normal_user.check_password(data["password"]))
-        
+
     def test_delete_user(self):
         # Test that an unauthenthicate user can't delete users data
         self.assertEqual(User.objects.count(), 2)
-        response = self.client.delete(f'/api/v1/users/{self.normal_user.id}/')
+        response = self.client.delete(f'{ENDPOINT}/{self.normal_user.id}/', format='json')
         self.assertEqual(response.status_code, 401)
         self.assertEqual(User.objects.count(), 2)
         
         # Test that an unverified user can't delete its data
         self.client.force_authenticate(user=self.normal_user)
-        response = self.client.delete(f'/api/v1/users/{self.normal_user.id}/')
+        response = self.client.delete(f'{ENDPOINT}/{self.normal_user.id}/', format='json')
         self.assertEqual(response.status_code, 403)
         self.assertEqual(User.objects.count(), 2)
         
         # Test that a verified user can't delete other's data
         self.normal_user.is_verified = True
         self.normal_user.save()
-        response = self.client.delete(f'/api/v1/users/{self.admin_user.id}/')
+        response = self.client.delete(f'{ENDPOINT}/{self.admin_user.id}/', format='json')
         self.assertEqual(response.status_code, 403)
         self.assertEqual(User.objects.count(), 2)
 
         # Test that a verified user can delete its data
-        response = self.client.delete(f'/api/v1/users/{self.normal_user.id}/')
+        response = self.client.delete(f'{ENDPOINT}/{self.normal_user.id}/', format='json')
         self.assertEqual(response.status_code, 204)
         self.assertEqual(User.objects.count(), 1)
 
     def test_log_in(self):
         testing_user = self._create_user(**{
-            'email': 'rightemail@appname.me',
+            'email': 'rightemail@notalone.me',
             'password': 'RightPassword'
         })
 
         # Test that cannot log in with an invalid email
         data = {
-            'email': 'wroongemail@appname.me',
+            'email': 'wroongemail@notalone.me',
             'password': 'RightPassword'
         }
-        response = self.client.post(f'/api/v1/users/login/', data)
+        response = self.client.post(f'{ENDPOINT}/login/', data, format='json')
         message = 'Invalid credentials'
         self.assertEqual(response.status_code, 400)
         self.assertTrue(message in response.data['non_field_errors'][0])
 
         # Test that cannot log in with an invalid password
         data = {
-            'email': 'rightemail@appname.me',
+            'email': 'rightemail@notalone.me',
             'password': 'WrongPassword'
         }
-        response = self.client.post(f'/api/v1/users/login/', data)
+        response = self.client.post(f'{ENDPOINT}/login/', data, format='json')
         message = 'Invalid credentials'
         self.assertEqual(response.status_code, 400)
         self.assertTrue(message in response.data['non_field_errors'][0])
 
         # Test that user not verified cannot log in
         data = {
-            'email': 'rightemail@appname.me',
+            'email': 'rightemail@notalone.me',
             'password': 'RightPassword'
         }
-        response = self.client.post(f'/api/v1/users/login/', data)
+        response = self.client.post(f'{ENDPOINT}/login/', data, format='json')
         message = 'User is not verified'
         self.assertEqual(response.status_code, 400)
         self.assertTrue(message in response.data['non_field_errors'][0])
@@ -401,10 +403,10 @@ class UserTests(UsersAbstractUtils):
         testing_user.is_verified = True
         testing_user.save()
         data = {
-            'email': 'rightemail@appname.me',
+            'email': 'rightemail@notalone.me',
             'password': 'RightPassword'
         }
-        response = self.client.post(f'/api/v1/users/login/', data)
+        response = self.client.post(f'{ENDPOINT}/login/', data, format='json')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content)
         self.assertTrue('token' in data)
@@ -418,11 +420,11 @@ class UserTests(UsersAbstractUtils):
         # request with it id and token
         token = self.normal_user.generate_verification_token()
         self.assertEqual(self.normal_user.is_verified, False)
-        response = self.client.get(f'/api/v1/users/{self.normal_user.id}/verificate/?token={token}')
+        response = self.client.get(f'{ENDPOINT}/{self.normal_user.id}/verificate/?token={token}')
         self.assertEqual(response.status_code, 202)
         normal_user_updated = User.objects.get(id=self.normal_user.id)
         self.assertEqual(normal_user_updated.is_verified, True)
-
+    
     def test_reset_password(self):
         # Test that any user can reset its password via API
         self.assertTrue(self.normal_user.check_password('password'))
@@ -431,7 +433,11 @@ class UserTests(UsersAbstractUtils):
         tokens = ResetPasswordToken.objects.all()
         self.assertEqual(len(tokens), 1)
         token = tokens[0].key
-        self.client.post(f'/api/v1/password_reset/confirm/', {'token': token, 'password': 'NewPassword95'})
+        data = {
+            'token': token, 
+            'password': 'NewPassword95'
+        }
+        self.client.post(f'/api/v1/password_reset/confirm/', data, format='json')
         self.assertEqual(response.status_code, 200)
         self.normal_user = User.objects.get(id=self.normal_user.id)
         self.assertTrue(self.normal_user.check_password('NewPassword95'))
