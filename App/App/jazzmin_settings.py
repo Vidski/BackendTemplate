c11c5126908269a3745ac7b243e3055f69a873fe
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2021-12-31 23:45:24 +0100

    Forms format changed in admin

--- App/App/jazzmin_settings.py
+++ App/App/jazzmin_settings.py
@@ -133,9 +133,12 @@ JAZZMIN_SETTINGS = {
     # - vertical_tabs
     # - collapsible
     # - carousel
-    "changeform_format": "horizontal_tabs",
+    "changeform_format": "vertical_tabs",
     # override change forms on a per modeladmin basis
-    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+    "changeform_format_overrides": {
+        # "auth.user": "collapsible",
+        # "auth.group": "vertical_tabs"
+    },
     # Add a language dropdown into the admin
     "language_chooser": False,
 }
