72f3faa2fecf0fd398054cf9a1dca45695859607
Alejandro <alejandroacho@hotmail.com>
2022-01-12 22:58:50 +0100

    Fakers and factories behaviour improved

--- App/Users/fakers/user_fakers.py
+++ App/Users/fakers/user_fakers.py
@@ -1,22 +1,13 @@
 import factory
 
-from Users.models import User
+from Users.factories.user_factories import UserFactory
 
 
-class UserFaker(factory.django.DjangoModelFactory):
-    class Meta:
-        model = User
-        django_get_or_create = ("email",)
+class UserFaker(UserFactory):
+    email = 'normaluser@appname.me'
 
-    first_name = factory.Faker("first_name")
-    last_name = factory.Faker("last_name")
-    email = factory.LazyAttribute(lambda user: f"{user.first_name}@appname.com")
-    password = factory.PostGenerationMethodCall('set_password', 'password')
-    is_admin = False
-    is_verified = False
-    phone_number = "123123123"
 
-class AdminFaker(UserFaker):
+class AdminFaker(UserFactory):
     email = 'adminuser@appname.me'
     is_admin = True
     is_verified = True
