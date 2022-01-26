d88612b0cb9b89ce55201e67b3e87630cb6c63c4
Alejandro <alejandroacho@hotmail.com>
2022-01-26 23:32:30 +0100

    Manage.py modified only set DJANGO_SETTINGS_MODULE if it was not setted on docker build

--- App/manage.py
+++ App/manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Settings.settings')
+
+    if not os.environ.get('DJANGO_SETTINGS_MODULE'):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Settings.local_settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
