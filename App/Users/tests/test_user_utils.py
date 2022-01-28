0fe42f62043dfbf968899c9bfa1efd22a5963b3b
Alejandro <alejandroacho@hotmail.com>
2022-01-28 21:01:45 +0100

    Get user or error util function tested

--- App/Users/tests/test_user_utils.py
+++ App/Users/tests/test_user_utils.py
@@ -1,11 +1,34 @@
+from rest_framework.exceptions import NotFound
 from rest_framework.exceptions import PermissionDenied
 
 from Users.factories.user_factories import UserFactory
 from Users.tests.abstract_test_classes import UsersAbstractUtils
+from Users.utils import get_user_or_error
 from Users.utils import verify_user_query_token
 
 
 class TestUsertUtils(UsersAbstractUtils):
+
+    def test_get_user_or_error_raises_NotFound_when_users_not_exists(self):
+        with self.assertRaises(NotFound):
+            get_user_or_error(1, 2)
+
+    def test_get_user_or_error_raises_PermissionDenied_looking_for_different_user(self):
+        with self.assertRaises(PermissionDenied):
+            get_user_or_error(self.normal_user, self.admin_user.id)
+
+    def test_get_user_or_error_returns_different_user_looking_for_it_as_admin(self):
+        instance = get_user_or_error(self.admin_user, self.normal_user.id)
+        self.assertEqual(instance, self.normal_user)
+
+    def test_get_user_or_error_raises_an_error_if_user_is_not_verified(self):
+        with self.assertRaises(PermissionDenied):
+            get_user_or_error(self.normal_user, self.normal_user.id)
+
+    def test_get_user_or_error_returns_its_user(self):
+        self.normal_user.verify()
+        instance = get_user_or_error(self.normal_user, self.normal_user.id)
+        self.assertEqual(instance, self.normal_user)
 
     def test_verify_user_query_token_raises_PermissionDenied(self):
         user = UserFactory()
