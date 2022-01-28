efa4ced265158bdb880ebd9e896a0a1cf05718fe
Alejandro <alejandroacho@hotmail.com>
2022-01-28 18:22:34 +0100

    More explicit comment over the CustomView

--- App/App/views.py
+++ App/App/views.py
@@ -7,7 +7,8 @@ class CustomView(TemplateView):
     This is a custom view included just to add a new page into
     the admin site. It can be removed if you don't need it, but
     take in mind that you will need to remove or comment the
-    function `manageHomePage` in the file `App/Static/admin_js.js`
+    function `manageHomePage` in the file `App/Static/admin_js.js`,
+    and `CustomView` in the App/urls.py file.
     """
     template_name = "../static/home.html"
 
