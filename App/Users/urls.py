761021a94e4833a0d03b27aa32153fc7a7ed7d83
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-02-04 22:07:18 +0100

    Imports refactored

--- App/Users/urls.py
+++ App/Users/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include
-
+from django.urls import include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from Users.views import UserViewSet
