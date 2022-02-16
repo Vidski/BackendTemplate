4fd9b25bf8188615586e57dc15b7d11e03c59ea1
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-02-16 23:38:09 +0100

    Email app registered

--- App/Emails/admin.py
+++ App/Emails/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from Emails.models import Email
+from Emails.models import Block
+
+
+class BlockAdmin(admin.ModelAdmin):
+    model = Block
+
+
+class EmailAdmin(admin.ModelAdmin):
+    model = Email
+
+
+admin.site.register(Email, EmailAdmin)
+admin.site.register(Block, BlockAdmin)
