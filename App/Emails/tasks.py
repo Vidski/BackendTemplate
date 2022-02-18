e03c19f24bef9b6f3a1b4a17546bdb5c1a3641c3
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-02-18 20:47:41 +0100

    Email task created

---
+++ App/Emails/tasks.py
@@ -0,0 +1,30 @@
+from celery import shared_task
+from django.utils import timezone
+
+from Emails.models import Email
+from  Worker.worker import app
+
+SECONDS = 10.0
+
+
+@shared_task
+def send_emails():
+    now = timezone.now()
+    emails = Email.objects.filter(
+        was_sent=False,
+        programed_send_date__lte=now
+    )
+    for email in emails:
+        email.send()
+
+
+def each_seconds():
+    return SECONDS
+
+
+app.conf.beat_schedule = {
+    'send_emails': {
+        'task': 'Emails.tasks.send_emails',
+        'schedule': each_seconds(),
+    },
+}
