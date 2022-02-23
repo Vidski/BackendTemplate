eabf4c36834911f7b6e94c8d214fc9eb80da34ee
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-02-23 23:31:34 +0100

    Static folder moved inside App folder

--- App/App/settings/django/local_settings.py
+++ App/App/settings/django/local_settings.py
@@ -13,7 +13,7 @@ ENVIRONMENT_NAME = 'dev'
 ALLOWED_HOSTS = []
 
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, '/App/static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Database
