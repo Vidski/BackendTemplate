b1e036c2da053502906a67604990fd2d692897e9
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-03-20 19:06:28 +0100

    Created serializer for suggestion email

---
+++ App/Emails/serializers.py
@@ -0,0 +1,23 @@
+from phonenumber_field.serializerfields import PhoneNumberField
+from rest_framework import serializers
+
+from Emails.models import Email
+
+
+class SuggestionEmailSerializer(serializers.Serializer):
+    """
+    Suggestion email serializer
+    """
+
+    email_id = serializers.IntegerField(source='id')
+    was_sent = serializers.BooleanField()
+    subject = serializers.CharField()
+    header = serializers.CharField()
+    blocks = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='content'
+    )
+
+    class Meta:
+        model = Email
