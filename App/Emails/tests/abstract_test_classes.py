718398d1daef8c6e65cc6c5643fb54ba2e3dd523
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-03-26 18:47:49 +0100

    User abstract class used for emails

--- App/Emails/tests/abstract_test_classes.py
+++ App/Emails/tests/abstract_test_classes.py
@@ -1,12 +1,12 @@
-from django.test import TestCase
-
 from Emails.models import Block
 from Emails.models import Email
+from Users.tests.abstract_test_classes import UsersAbstractUtils
 
 
-class EmailsAbstractUtils(TestCase):
+class EmailsAbstractUtils(UsersAbstractUtils):
     def setUp(self):
         self._clean()
+        super().setUp()
 
     def _clean(self):
         Block.objects.all().delete()
