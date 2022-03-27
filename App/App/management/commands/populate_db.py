184cbeeecfa09f2603803c147a224a1627c4e65d
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-03-27 03:01:10 +0200

    Command refactored and email added to population

--- App/App/management/commands/populate_db.py
+++ App/App/management/commands/populate_db.py
@@ -2,8 +2,10 @@ import logging
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from tqdm import tqdm
 from tqdm import trange as progress
 
+from Emails.factories.email import VerifyEmailFactory
 from Users.factories.user import UserFactory
 
 
@@ -16,14 +18,30 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if settings.ENVIRONMENT_NAME in ['dev', 'local', 'test']:
-            self.populate_users()
+            self.populate()
         else:
             logger.critical(
-                'This command creates fake data do NOT run this in production environments'
+                'This command creates fake data do NOT run this in'
+                + ' production environments'
             )
 
-    def populate_users(self):
+    def populate(self):
+        users = self.create_fake_users()
+        self.create_fake_verify_emails(users)
+
+    def create_fake_users(self):
         self.stdout.write('Creating fake users')
+        users = []
         for _ in progress(50):
-            UserFactory()
+            user = UserFactory()
+            users.append(user)
+        self.stdout.write('Fake users created')
+        return users
+
+    def create_fake_verify_emails(self, users):
+        self.stdout.write('Creating fake verify emails')
+        with tqdm(total=len(users)) as progress_bar:
+            for user in users:
+                VerifyEmailFactory(instance=user)
+                progress_bar.update(1)
         self.stdout.write('Fake users created')
