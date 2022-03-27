6c71767ff414c34d5f248c9c24e7426523bb5899
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-03-27 01:13:46 +0100

    Block and email admin improved

--- App/Emails/admin.py
+++ App/Emails/admin.py
@@ -6,10 +6,46 @@ from Emails.models import Email
 
 class BlockAdmin(admin.ModelAdmin):
     model = Block
+    list_display = ('id', 'title', 'show_link')
+    list_display_links = ('id', 'title')
+    list_filter = (
+        'title',
+        'show_link',
+    )
+    fieldsets = (
+        ('Content', {'fields': ('id', 'title', 'content')}),
+        ('Link', {'fields': ('show_link', 'link_text', 'link')}),
+    )
+    readonly_fields = [
+        'id',
+    ]
+    search_fields = ('title', 'id', 'link', 'link_text', 'content')
 
 
 class EmailAdmin(admin.ModelAdmin):
     model = Email
+    list_display = (
+        'id',
+        'subject',
+        'to',
+        'to_all_users',
+        'is_test',
+        'was_sent',
+    )
+    list_filter = ('to', 'to_all_users', 'is_test', 'was_sent')
+    fieldsets = (
+        ('Content', {'fields': ('id', 'subject', 'header', 'to')}),
+        ('Blocks', {'fields': ('blocks',)}),
+        (
+            'Configuration',
+            {'fields': ('to_all_users', 'is_test', 'programed_send_date')},
+        ),
+        ('Sent information', {'fields': ('was_sent', 'sent_date')}),
+    )
+    list_display_links = ('id', 'subject')
+    readonly_fields = ['id', 'was_sent', 'sent_date']
+    search_fields = ('to', 'id', 'subject', 'programed_send_date')
+    ordering = ('to_all_users', 'is_test', 'was_sent', 'sent_date', 'to')
 
 
 admin.site.register(Email, EmailAdmin)
