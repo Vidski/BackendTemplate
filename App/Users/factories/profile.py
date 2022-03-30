b3b3ae7b59494106188217dbcc65c3472da83078
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-03-30 23:45:49 +0200

    Profile factory created

---
+++ App/Users/factories/profile.py
@@ -0,0 +1,23 @@
+import factory
+import factory.fuzzy
+
+from Users.factories.user import UserFactory
+from Users.models import Profile
+
+
+class ProfileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Profile
+
+    user = factory.SubFactory(UserFactory)
+    nickname = factory.Faker('last_name')
+    bio = factory.Faker('text')
+    profile_image = factory.django.ImageField(
+        filename="profile_picture.jpg",
+        width=100,
+        height=100,
+        format="JPEG",
+        color="gray",
+    )
+    gender = factory.fuzzy.FuzzyChoice(('F', 'M', 'N', 'P'))
+    birth_date = factory.Faker('date')
