1d8aeb8ffe593dd882bb7f0bd3af0fcfb3ffed1b
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-04-07 01:05:52 +0200

    Email utils test passed to pytest

--- App/Emails/tests/test_utils.py
+++ App/Emails/tests/test_utils.py
@@ -1,5 +1,6 @@
+import pytest
+
 from django.core import mail
-from django.test import TestCase
 from django_rest_passwordreset.models import ResetPasswordToken
 
 from Emails.models import Email
@@ -7,26 +8,27 @@ from Emails.utils import send_email
 from Users.fakers.user import UserFaker
 
 
-class TestEmailUtils(TestCase):
+@pytest.mark.django_db
+class TestEmailUtils:
     def test_send_email_verify_email(self):
         email_type = 'verify_email'
         user = UserFaker()
         emails = Email.objects.all().count()
-        self.assertEqual(emails, 0)
-        self.assertEqual(len(mail.outbox), 0)
+        assert emails == 0
+        assert len(mail.outbox) == 0
         send_email(email_type, user)
         emails = Email.objects.all().count()
-        self.assertEqual(emails, 1)
-        self.assertEqual(len(mail.outbox), 1)
+        assert emails == 1
+        assert len(mail.outbox) == 1
 
     def test_reset_password_verify_email(self):
         email_type = 'reset_password'
         user = UserFaker()
         instance = ResetPasswordToken.objects.create(user=user)
         emails = Email.objects.all().count()
-        self.assertEqual(emails, 0)
-        self.assertEqual(len(mail.outbox), 0)
+        assert emails == 0
+        assert len(mail.outbox) == 0
         send_email(email_type, instance)
         emails = Email.objects.all().count()
-        self.assertEqual(emails, 1)
-        self.assertEqual(len(mail.outbox), 1)
+        assert emails == 1
+        assert len(mail.outbox) == 1
