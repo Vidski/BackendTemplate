6b8d0d8f6f038e2bd4d8d235682a13f034c9e633
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-04-07 01:03:13 +0200

    Email views test passed to pytest

--- App/Emails/tests/test_views.py
+++ App/Emails/tests/test_views.py
@@ -1,29 +1,33 @@
+import pytest
+
 from django.core import mail
 
 from Emails.models import Email
-from Emails.tests.abstract_test_classes import EmailsAbstractUtils
+from Users.fakers.user import VerifiedUserFaker
 
 
 ENDPOINT = '/api/v1/emails/suggestion/'
 
 
-class TestSuggestionViews(EmailsAbstractUtils):
-    def test_suggestion_fails_as_unauthenticate_user(self):
+@pytest.mark.django_db
+class TestSuggestionViews:
+    def test_suggestion_fails_as_unauthenticate_user(self, client):
         assert len(mail.outbox) == 0
         data = {'type': 'Error', 'content': 'Error found'}
-        response = self.client.post(ENDPOINT, data, format='json')
+        response = client.post(ENDPOINT, data, format='json')
         assert response.status_code == 401
         assert len(mail.outbox) == 0
 
-    def test_suggestion_creates_email_as_authenticate_user(self):
+    def test_suggestion_creates_email_as_authenticate_user(self, client):
+        normal_user = VerifiedUserFaker()
         email_count = Email.objects.all().count()
         assert len(mail.outbox) == 0
         assert email_count == 0
         data = {'type': 'Error', 'content': 'Error found'}
-        self.client.force_authenticate(user=self.normal_user)
-        response = self.client.post(ENDPOINT, data, format='json')
+        client.force_authenticate(user=normal_user)
+        response = client.post(ENDPOINT, data, format='json')
         email_count = Email.objects.all().count()
-        expected_header = f'Error from user with id: {self.normal_user.id}'
+        expected_header = f'Error from user with id: {normal_user.id}'
         assert response.status_code == 201
         assert True == response.data['was_sent']
         assert 'Error' == response.data['subject']
@@ -32,13 +36,16 @@ class TestSuggestionViews(EmailsAbstractUtils):
         assert len(mail.outbox) == 0
         assert email_count == 1
 
-    def test_suggestion_fails_as_authenticate_user_because_wrong_type(self):
+    def test_suggestion_fails_as_authenticate_user_because_wrong_type(
+        self, client
+    ):
+        normal_user = VerifiedUserFaker()
         email_count = Email.objects.all().count()
         assert len(mail.outbox) == 0
         assert email_count == 0
         data = {'type': 'Wrong', 'content': 'Error found'}
-        self.client.force_authenticate(user=self.normal_user)
-        response = self.client.post(ENDPOINT, data, format='json')
+        client.force_authenticate(user=normal_user)
+        response = client.post(ENDPOINT, data, format='json')
         email_count = Email.objects.all().count()
         expected_error_message = 'Invalid type of suggestion'
         assert response.status_code == 400
