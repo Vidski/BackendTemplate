74ba99fa5610a9e785e882f7dcaeab9ae8600978
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-04-09 23:45:51 +0200

    Profile fakers tested

--- App/Users/tests/test_fakers.py
+++ App/Users/tests/test_fakers.py
@@ -1,5 +1,11 @@
 import pytest
 
+from Users.fakers.profile import AdultProfileFaker
+from Users.fakers.profile import FemaleProfileFaker
+from Users.fakers.profile import KidProfileFaker
+from Users.fakers.profile import MaleProfileFaker
+from Users.fakers.profile import NonBinaryProfileFaker
+from Users.fakers.profile import NotSaidProfileFaker
 from Users.fakers.user import AdminFaker
 from Users.fakers.user import UserFaker
 from Users.fakers.user import VerifiedUserFaker
@@ -51,3 +57,84 @@ class TestUserFakers:
         assert user.check_password('password') is True
         assert user.is_admin is True
         assert user.is_verified is True
+
+
+@pytest.mark.django_db
+class TestProfileFakers:
+    def test_adult_profile_faker(self):
+        assert Profile.objects.count() == 0
+        profile = AdultProfileFaker()
+        assert Profile.objects.count() == 1
+        assert profile.user is not None
+        assert profile.nickname is not None
+        assert profile.bio == 'Custom bio for adult profile'
+        assert profile.gender is not None
+        assert profile.birth_date is not None
+        assert profile.is_adult() is True
+        assert profile.profile_image is not None
+        assert profile.profile_image.url is not None
+
+    def test_kid_profile_faker(self):
+        assert Profile.objects.count() == 0
+        profile = KidProfileFaker()
+        assert Profile.objects.count() == 1
+        assert profile.user is not None
+        assert profile.nickname is not None
+        assert profile.bio == 'Custom bio for kid profile'
+        assert profile.gender is not None
+        assert profile.birth_date is not None
+        assert profile.is_adult() is False
+        assert profile.profile_image is not None
+        assert profile.profile_image.url is not None
+
+    def test_female_profile_faker(self):
+        assert Profile.objects.count() == 0
+        profile = FemaleProfileFaker()
+        assert Profile.objects.count() == 1
+        assert profile.user is not None
+        assert profile.nickname is not None
+        assert profile.bio == 'Custom bio for female profile'
+        assert profile.gender is not None
+        assert profile.gender == 'F'
+        assert profile.birth_date is not None
+        assert profile.profile_image is not None
+        assert profile.profile_image.url is not None
+
+    def test_male_profile_faker(self):
+        assert Profile.objects.count() == 0
+        profile = MaleProfileFaker()
+        assert Profile.objects.count() == 1
+        assert profile.user is not None
+        assert profile.nickname is not None
+        assert profile.bio == 'Custom bio for male profile'
+        assert profile.gender is not None
+        assert profile.gender == 'M'
+        assert profile.birth_date is not None
+        assert profile.profile_image is not None
+        assert profile.profile_image.url is not None
+
+    def test_non_binary_profile_faker(self):
+        assert Profile.objects.count() == 0
+        profile = NonBinaryProfileFaker()
+        assert Profile.objects.count() == 1
+        assert profile.user is not None
+        assert profile.nickname is not None
+        assert profile.bio == 'Custom bio for non-binary profile'
+        assert profile.gender is not None
+        assert profile.gender == 'N'
+        assert profile.birth_date is not None
+        assert profile.profile_image is not None
+        assert profile.profile_image.url is not None
+
+    def test_not_said_profile_faker(self):
+        assert Profile.objects.count() == 0
+        profile = NotSaidProfileFaker()
+        assert Profile.objects.count() == 1
+        assert profile.user is not None
+        assert profile.nickname is not None
+        assert profile.bio == 'Custom bio for x profile'
+        assert profile.gender is not None
+        assert profile.gender == 'P'
+        assert profile.birth_date is not None
+        assert profile.profile_image is not None
+        assert profile.profile_image.url is not None
