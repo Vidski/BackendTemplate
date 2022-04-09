1569f2c29c5804147c798870abd262ed5beb9d5f
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-04-09 23:29:59 +0200

    Profile factory tested

--- App/Users/tests/test_factories.py
+++ App/Users/tests/test_factories.py
@@ -26,3 +26,18 @@ class TestUserFactory:
         assert user.check_password('password') is True
         assert user.is_admin is False
         assert user.is_verified is False
+
+
+@pytest.mark.django_db
+class TestProfileFactory:
+    def test_profile_factory(self):
+        assert Profile.objects.count() == 0
+        profile = ProfileFactory()
+        assert Profile.objects.count() == 1
+        assert profile.user is not None
+        assert profile.nickname is not None
+        assert profile.bio is not None
+        assert profile.gender is not None
+        assert profile.birth_date is not None
+        assert profile.profile_image is not None
+        assert profile.profile_image.url is not None
