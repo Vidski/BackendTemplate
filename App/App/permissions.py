0513e3b78c4293bb4f5ac67ab5a05d5f6a698ce6
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-04-10 23:29:26 +0200

    Permissions created and tested

---
+++ App/App/permissions.py
@@ -0,0 +1,30 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import BasePermission
+
+from Users.models import User
+
+
+class IsAdmin(BasePermission):
+    message = 'You don\'t have permission'
+
+    def has_permission(self, request, view):
+        return request.user.is_admin
+
+
+class IsVerified(BasePermission):
+    message = 'You have to verify your account first'
+
+    def has_permission(self, request, view):
+        return request.user.is_verified
+
+
+class IsUserOwner(BasePermission):
+    message = 'You don\'t have permission'
+
+    def has_permission(self, request, view):
+        try:
+            pk = request.parser_context['kwargs']['pk']
+            user = get_object_or_404(User, id=pk)
+        except:
+            return False
+        return request.user.has_permission(user)
---
+++ App/App/tests/test_permissions.py
@@ -0,0 +1,69 @@
+import pytest
+from mock import MagicMock
+from mock import PropertyMock
+
+from App.permissions import IsAdmin
+from App.permissions import IsUserOwner
+from App.permissions import IsVerified
+from Users.fakers.user import AdminFaker
+from Users.fakers.user import UserFaker
+from Users.fakers.user import VerifiedUserFaker
+
+@pytest.mark.django_db
+class TestIsAdminPermission:
+    def test_returns_false_if_user_is_not_admin(self):
+        requester = UserFaker()
+        assert requester.is_admin is False
+        request = MagicMock()
+        mocked_requester = PropertyMock(return_value=requester)
+        type(request).user = mocked_requester
+        assert IsAdmin().has_permission(request, None) is False
+
+    def test_returns_true_if_user_is_admin(self):
+        requester = AdminFaker()
+        assert requester.is_admin is True
+        request = MagicMock()
+        mocked_requester = PropertyMock(return_value=requester)
+        type(request).user = mocked_requester
+        assert IsAdmin().has_permission(request, None) is True
+
+@pytest.mark.django_db
+class TestIsVerifiedPermission:
+    def test_returns_false_if_user_is_not_verified(self):
+        requester = UserFaker()
+        assert requester.is_verified is False
+        request = MagicMock()
+        mocked_requester = PropertyMock(return_value=requester)
+        type(request).user = mocked_requester
+        assert IsVerified().has_permission(request, None) is False
+
+    def test_returns_true_if_user_is_verified(self):
+        requester = VerifiedUserFaker()
+        assert requester.is_verified is True
+        request = MagicMock()
+        mocked_requester = PropertyMock(return_value=requester)
+        type(request).user = mocked_requester
+        assert IsVerified().has_permission(request, None) is True
+
+@pytest.mark.django_db
+class TestIsUserOwnerPermission:
+    def test_returns_false_if_user_is_not_owner(self):
+        user = UserFaker()
+        requester = VerifiedUserFaker()
+        request = MagicMock()
+        kwargs = {'kwargs':{'pk': user.id}}
+        mocked_kwargs = PropertyMock(return_value=kwargs)
+        type(request).parser_context = mocked_kwargs
+        mocked_requester = PropertyMock(return_value=requester)
+        type(request).user = mocked_requester
+        assert IsUserOwner().has_permission(request, None) is False
+
+    def test_returns_true_if_user_is_owner(self):
+        requester = VerifiedUserFaker()
+        request = MagicMock()
+        kwargs = {'kwargs':{'pk': requester.id}}
+        mocked_kwargs = PropertyMock(return_value=kwargs)
+        mocked_requester = PropertyMock(return_value=requester)
+        type(request).user = mocked_requester
+        type(request).parser_context = mocked_kwargs
+        assert IsUserOwner().has_permission(request, None) is True
