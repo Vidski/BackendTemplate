a0af48604c58a5007da8e1659f88d885ebace9e7
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-04-19 22:20:20 +0200

    Suggestion model refactored

--- App/Emails/models.py
+++ App/Emails/models.py
@@ -28,22 +28,14 @@ class AbstractEmailClass(models.Model):
     class Meta:
         abstract = True
 
-
     def __str__(self):
         return f'{self.id} | {self.subject}'
 
-    def save(self, *args, **kwargs):
-        self.to = ', '.join(self.get_emails())
-        if self.programed_send_date is None:
-            now = timezone.now()
-            five_minutes_from_now = now + timezone.timedelta(minutes=5)
-            self.programed_send_date = five_minutes_from_now
-        super(AbstractEmailClass, self).save(*args, **kwargs)
-
     def get_emails(self):
-        if self.to_all_users and not self.is_test:
+        is_test = getattr(self, "is_test", None)
+        if getattr(self, "to_all_users", None) and not is_test:
             return [f'{user.email}' for user in User.objects.all()]
-        if self.is_test:
+        if is_test:
             return [f'{settings.TEST_EMAIL}']
         emails = self.to.split(', ')
         return [email.strip() for email in emails]
@@ -92,12 +84,20 @@ class Email(AbstractEmailClass):
     sent_date = models.DateTimeField(null=True)
     was_sent = models.BooleanField(default=False, editable=False)
 
+    def save(self, *args, **kwargs):
+        self.to = ', '.join(self.get_emails())
+        if self.programed_send_date is None:
+            now = timezone.now()
+            five_minutes_from_now = now + timezone.timedelta(minutes=5)
+            self.programed_send_date = five_minutes_from_now
+        super(Email, self).save(*args, **kwargs)
+
 
 class Suggestion(AbstractEmailClass):
     """
     Suggestion model
     """
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='suggestion',
     )
     subject = models.CharField(
@@ -110,3 +110,4 @@ class Suggestion(AbstractEmailClass):
     to = models.TextField(null=True, )
     sent_date = models.DateTimeField(null=True)
     was_sent = models.BooleanField(default=False, editable=False)
+    was_read = models.BooleanField(default=False)
