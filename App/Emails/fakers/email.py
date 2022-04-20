b40c55d3b869fe41457ebe06d3bc13ba173ec63e
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-04-21 00:44:03 +0200

    Suggestion faker created and tested

--- App/Emails/fakers/email.py
+++ App/Emails/fakers/email.py
@@ -1,7 +1,9 @@
 import factory
 
 from Emails.factories.email import EmailFactory
+from Emails.factories.email import SuggestionEmailFactory
 from Emails.fakers.block import BlockTestFaker
+from Users.factories.user import UserFactory
 
 
 class EmailTestFaker(EmailFactory):
@@ -13,6 +15,19 @@ class EmailTestFaker(EmailFactory):
     programed_send_date = None
     sent_date = None
     was_sent = False
+
+    @factory.post_generation
+    def blocks(self, create, extracted, **kwargs):
+        self.blocks.add(BlockTestFaker())
+
+
+class SuggestionErrorFaker(SuggestionEmailFactory):
+    subject = 'Test subject'
+    header = 'Test header'
+    sent_date = None
+    was_sent = False
+    was_read = False
+    user = factory.SubFactory(UserFactory)
 
     @factory.post_generation
     def blocks(self, create, extracted, **kwargs):
--- App/Emails/tests/test_fakers.py
+++ App/Emails/tests/test_fakers.py
@@ -3,8 +3,10 @@ from django.conf import settings
 
 from Emails.fakers.block import BlockTestFaker
 from Emails.fakers.email import EmailTestFaker
+from Emails.fakers.email import SuggestionErrorFaker
 from Emails.models import Block
 from Emails.models import Email
+from Emails.models import Suggestion
 
 
 @pytest.mark.django_db
@@ -35,6 +37,25 @@ class TestBlockFakers:
         assert Block.objects.count() == 0
         block = BlockTestFaker()
         assert Block.objects.count() == 1
+        assert block.title == 'test'
+        assert block.content == 'test'
+        assert block.show_link == True
+        assert block.link_text == 'test'
+        assert block.link == 'test.com'
+
+@pytest.mark.django_db
+class TestBlockFakers:
+    def test_block_faker_creates_block(self):
+        assert Block.objects.count() == 0
+        assert Suggestion.objects.count() == 0
+        suggestion = SuggestionErrorFaker()
+        assert Block.objects.count() == 1
+        assert Suggestion.objects.count() == 1
+        assert suggestion.subject == 'Test subject'
+        assert suggestion.header == 'Test header'
+        assert suggestion.to == f'{settings.SUGGESTIONS_EMAIL}'
+        assert suggestion.blocks.first() is not None
+        block = suggestion.blocks.first()
         assert block.title == 'test'
         assert block.content == 'test'
         assert block.show_link == True
