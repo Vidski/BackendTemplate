e23351d6df1f81da55a506c7a7dd7717750c5faf
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-05-06 00:33:37 +0200

    Test settings refactor to avoid having duplicated databases

--- App/App/settings/django/test_settings.py
+++ App/App/settings/django/test_settings.py
@@ -5,18 +5,3 @@ ENVIRONMENT_NAME = 'test'
 
 STATICFILES_DIRS = ()
 STATIC_ROOT = os.path.join(BASE_DIR, 'Static')
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test_database',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': 'database',  # <-- docker host name for db
-        'PORT': '3306',  # <-- docker port for db
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'sql_mode': 'STRICT_TRANS_TABLES',
-        },
-    }
-}
