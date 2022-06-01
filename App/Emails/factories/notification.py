825d2d389c35e402fc3b3b9e0c52ed6f24d8db1d
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-06-01 22:52:57 +0200

    Notification faker and factory fixed

--- App/Emails/factories/notification.py
+++ App/Emails/factories/notification.py
@@ -3,7 +3,6 @@ from django.utils import timezone
 
 from Emails.factories.block import BlockFactory
 from Emails.models.models import Notification
-from Users.factories.user import UserFactory
 
 
 class NotificationFactory(factory.django.DjangoModelFactory):
@@ -14,15 +13,11 @@ class NotificationFactory(factory.django.DjangoModelFactory):
     header = factory.Faker('word')
     is_test = False
     programed_send_date = timezone.now() + timezone.timedelta(minutes=10)
-    sent_date = factory.Faker(
-        'date_time', tzinfo=timezone.get_current_timezone()
-    )
+    sent_date = None
     was_sent = False
 
     @factory.post_generation
     def blocks(self, create, extracted, **kwargs):
-        if not create:
-            return
         if extracted:
             for block in extracted:
                 self.blocks.add(block)
