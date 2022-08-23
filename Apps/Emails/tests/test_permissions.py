7b237d302cd9a1497e802c81a57e2f9514f537e2
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-08-23 23:53:19 +0200

    Remove tests for removed permission

--- Apps/Emails/tests/test_permissions.py
+++ Apps/Emails/tests/test_permissions.py
@@ -5,7 +5,6 @@ from mock import PropertyMock
 
 from Emails.fakers.blacklist import BlackListTestFaker
 from Emails.models.models import BlackList
-from Emails.permissions import HasListBlacklistPermission
 from Emails.permissions import IsBlacklistOwner
 from Users.fakers.user import AdminFaker
 from Users.fakers.user import UserFaker
@@ -46,86 +45,3 @@ class TestIsBlacklistOwnerPermission:
         type(request).parser_context = mocked_kwargs
         with pytest.raises(Http404):
             IsBlacklistOwner().has_permission(request, None)
-
-
-@pytest.mark.django_db
-class TestHasListBlacklistPermission:
-    def test_is_a_list_request_returns_true(self) -> None:
-        blacklist: BlackList = BlackListTestFaker()
-        kwargs: dict = {"kwargs": {"pk": blacklist.id}}
-        requester: User = UserFaker()
-        request: MagicMock = MagicMock()
-        mocked_kwargs: PropertyMock = PropertyMock(return_value=kwargs)
-        mocked_requester: PropertyMock = PropertyMock(return_value=requester)
-        mocker_method: PropertyMock = PropertyMock(return_value="GET")
-        type(request).user = mocked_requester
-        type(request).parser_context = mocked_kwargs
-        type(request).method = mocker_method
-        assert HasListBlacklistPermission().is_a_list_request(request) is False
-
-    def test_is_a_list_request_returns_false(self) -> None:
-        requester: User = UserFaker()
-        request: MagicMock = MagicMock()
-        mocked_requester: PropertyMock = PropertyMock(return_value=requester)
-        mocker_method: PropertyMock = PropertyMock(return_value="GET")
-        type(request).user = mocked_requester
-        type(request).method = mocker_method
-        assert HasListBlacklistPermission().is_a_list_request(request) is False
-
-    def test_returns_true_if_user_is_retrieving_an_element(self) -> None:
-        blacklist: BlackList = BlackListTestFaker()
-        kwargs: dict = {"kwargs": {"pk": blacklist.id}}
-        requester: User = UserFaker()
-        request: MagicMock = MagicMock()
-        mocked_kwargs: PropertyMock = PropertyMock(return_value=kwargs)
-        mocked_requester: PropertyMock = PropertyMock(return_value=requester)
-        mocker_method: PropertyMock = PropertyMock(return_value="GET")
-        type(request).user = mocked_requester
-        type(request).parser_context = mocked_kwargs
-        type(request).method = mocker_method
-        assert (
-            HasListBlacklistPermission().has_permission(request, None) is True
-        )
-
-    def test_returns_false_if_user_is_listing_elements(self) -> None:
-        kwargs: dict = {"kwargs": {}}
-        requester: User = UserFaker()
-        request: MagicMock = MagicMock()
-        mocked_kwargs: PropertyMock = PropertyMock(return_value=kwargs)
-        mocked_requester: PropertyMock = PropertyMock(return_value=requester)
-        mocker_method: PropertyMock = PropertyMock(return_value="GET")
-        type(request).user = mocked_requester
-        type(request).parser_context = mocked_kwargs
-        type(request).method = mocker_method
-        assert (
-            HasListBlacklistPermission().has_permission(request, None) is False
-        )
-
-    def test_returns_true_if_admin_is_listing_elements(self) -> None:
-        kwargs: dict = {"kwargs": {}}
-        requester: User = AdminFaker()
-        request: MagicMock = MagicMock()
-        mocked_kwargs: PropertyMock = PropertyMock(return_value=kwargs)
-        mocked_requester: PropertyMock = PropertyMock(return_value=requester)
-        mocker_method: PropertyMock = PropertyMock(return_value="GET")
-        type(request).user = mocked_requester
-        type(request).parser_context = mocked_kwargs
-        type(request).method = mocker_method
-        assert (
-            HasListBlacklistPermission().has_permission(request, None) is True
-        )
-
-    def test_returns_true_if_admin_is_retrieving_an_element(self) -> None:
-        blacklist: BlackList = BlackListTestFaker()
-        kwargs: dict = {"kwargs": {"pk": blacklist.id}}
-        requester: User = AdminFaker()
-        request: MagicMock = MagicMock()
-        mocked_kwargs: PropertyMock = PropertyMock(return_value=kwargs)
-        mocked_requester: PropertyMock = PropertyMock(return_value=requester)
-        mocker_method: PropertyMock = PropertyMock(return_value="GET")
-        type(request).user = mocked_requester
-        type(request).parser_context = mocked_kwargs
-        type(request).method = mocker_method
-        assert (
-            HasListBlacklistPermission().has_permission(request, None) is True
-        )
