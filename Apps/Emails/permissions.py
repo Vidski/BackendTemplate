e497ac516b43948809262378e15a3f9bb0a80794
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-09-06 23:14:58 +0200

    Add methods that handles data into permissions

--- Apps/Emails/permissions.py
+++ Apps/Emails/permissions.py
@@ -20,6 +20,7 @@ class IsBlacklistOwner(BasePermission):
 
 class HasBlacklistPetitionPermission(IsBlacklistOwner):
     message: str = "You don't have permission to perform this action"
+    HANDLE_BLACKLIST_METHODS: list = ["POST", "PUT", "PATCH"]
 
     def is_a_list_type_request(self, request: HttpRequest) -> bool:
         is_get_method: bool = request.method == "GET"
@@ -29,7 +30,7 @@ class HasBlacklistPetitionPermission(IsBlacklistOwner):
     def has_permission(self, request: HttpRequest, view: View) -> bool:
         if self.is_a_list_type_request(request):
             return False
-        if request.method == "POST":
+        if request.method in self.HANDLE_BLACKLIST_METHODS:
             id_in_body: int = int(request.data["user"])
             return id_in_body == request.user.id
         return super().has_permission(request, view)
