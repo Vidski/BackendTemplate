daea3c1bfb646cedc09524ba10f4d823b5059f44
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-09-13 00:00:10 +0200

    Add notification endpoints

--- Apps/Emails/tests/test_views.py
+++ Apps/Emails/tests/test_views.py
@@ -8,7 +8,11 @@ from Emails.choices import CommentType
 from Emails.factories.blacklist import BlackListFactory
 from Emails.factories.suggestion import SuggestionEmailFactory
 from Emails.fakers.blacklist import BlackListTestFaker
+from Emails.fakers.notification import NotificationTestFaker
 from Emails.models.models import BlackList
+from Emails.models.models import Block
+from Emails.models.models import Email
+from Emails.models.models import Notification
 from Emails.models.models import Suggestion
 from Users.fakers.user import AdminFaker
 from Users.fakers.user import UserFaker
@@ -504,3 +508,395 @@ class TestDeleteBlacklistViews:
         client.force_authenticate(user=admin)
         response: Response = client.delete(url)
         assert response.status_code == 204
+
+
+@pytest.mark.django_db
+class TestCreateNotificationsView:
+    def url(self) -> str:
+        return reverse("emails:notifications-list")
+
+    def test_url(self) -> None:
+        assert self.url() == "/api/notifications/"
+
+    def setup_method(self) -> None:
+        Email.objects.all().delete()
+        Block.objects.all().delete()
+        Notification.objects.all().delete()
+
+    def test_post_notification_fails_as_unauthenticated(
+        self, client: APIClient
+    ) -> None:
+        data: dict = {
+            "header": "Email header",
+            "affair": "NOTIFICATION",
+            "subject": "Email subject",
+            "is_test": True,
+            "programed_send_date": "2050-12-12T10:59.000",
+            "blocks": [
+                {
+                    "title": "Block title",
+                    "content": "Block content",
+                    "link_text": "Click here",
+                    "show_link": True,
+                    "link": "google.com",
+                }
+            ],
+        }
+        url: str = self.url()
+        response: Response = client.post(url, data=data, format="json")
+        assert response.status_code == 401
+
+    def test_post_notification_fails_with_user_unverified(
+        self, client: APIClient
+    ) -> None:
+        user: User = UserFaker()
+        data: dict = {
+            "header": "Email header",
+            "affair": "NOTIFICATION",
+            "subject": "Email subject",
+            "is_test": True,
+            "programed_send_date": "2050-12-12T10:59.000",
+            "blocks": [
+                {
+                    "title": "Block title",
+                    "content": "Block content",
+                    "link_text": "Click here",
+                    "show_link": True,
+                    "link": "google.com",
+                }
+            ],
+        }
+        url: str = self.url()
+        client.force_authenticate(user=user)
+        response: Response = client.post(url, data=data, format="json")
+        assert response.status_code == 403
+
+    def test_post_notification_fails_with_verified_user(
+        self, client: APIClient
+    ) -> None:
+        user: User = VerifiedUserFaker()
+        data: dict = {
+            "header": "Email header",
+            "affair": "NOTIFICATION",
+            "subject": "Email subject",
+            "is_test": True,
+            "programed_send_date": "2050-12-12T10:59.000",
+            "blocks": [
+                {
+                    "title": "Block title",
+                    "content": "Block content",
+                    "link_text": "Click here",
+                    "show_link": True,
+                    "link": "google.com",
+                }
+            ],
+        }
+        url: str = self.url()
+        client.force_authenticate(user=user)
+        response: Response = client.post(url, data=data, format="json")
+        assert response.status_code == 403
+
+    def test_post_notification_works_with_admin(
+        self, client: APIClient
+    ) -> None:
+        user: User = AdminFaker()
+        data: dict = {
+            "header": "Email header",
+            "affair": "NOTIFICATION",
+            "subject": "Email subject",
+            "is_test": True,
+            "programed_send_date": "2050-12-12T10:59.000",
+            "blocks": [
+                {
+                    "title": "Block title",
+                    "content": "Block content",
+                    "link_text": "Click here",
+                    "show_link": True,
+                    "link": "google.com",
+                },
+                {
+                    "title": "Block title 2",
+                    "content": "Block content 2",
+                    "show_link": False,
+                },
+            ],
+        }
+        url: str = self.url()
+        client.force_authenticate(user=user)
+        assert Notification.objects.count() == 0
+        assert Block.objects.count() == 0
+        response: Response = client.post(url, data=data, format="json")
+        assert Notification.objects.count() == 1
+        assert Block.objects.count() == 2
+        assert Notification.objects.first().blocks.count() == 2
+        assert response.status_code == 201
+
+
+@pytest.mark.django_db
+class TestListNotificationsView:
+    def url(self) -> str:
+        return reverse("emails:notifications-list")
+
+    def test_url(self) -> None:
+        assert self.url() == "/api/notifications/"
+
+    def setup_method(self) -> None:
+        Email.objects.all().delete()
+        Block.objects.all().delete()
+        Notification.objects.all().delete()
+
+    def test_post_notification_fails_as_unauthenticated(
+        self, client: APIClient
+    ) -> None:
+        url: str = self.url()
+        response: Response = client.get(url)
+        assert response.status_code == 401
+
+    def test_post_notification_fails_with_user_unverified(
+        self, client: APIClient
+    ) -> None:
+        user: User = UserFaker()
+        url: str = self.url()
+        client.force_authenticate(user=user)
+        response: Response = client.get(url)
+        assert response.status_code == 403
+
+    def test_post_notification_fails_with_verified_user(
+        self, client: APIClient
+    ) -> None:
+        user: User = VerifiedUserFaker()
+        url: str = self.url()
+        client.force_authenticate(user=user)
+        response: Response = client.get(url)
+        assert response.status_code == 403
+
+    def test_post_notification_works_with_admin(
+        self, client: APIClient
+    ) -> None:
+        user: User = AdminFaker()
+        notification: Notification = NotificationTestFaker()
+        url: str = self.url()
+        client.force_authenticate(user=user)
+        response: Response = client.get(url)
+        assert response.status_code == 200
+        assert notification.id == response.data["results"][0]["id"]
+
+
+@pytest.mark.django_db
+class TestListNotificationsView:
+    def url(self) -> str:
+        return reverse("emails:notifications-list")
+
+    def test_url(self) -> None:
+        assert self.url() == "/api/notifications/"
+
+    def setup_method(self) -> None:
+        Email.objects.all().delete()
+        Block.objects.all().delete()
+        Notification.objects.all().delete()
+
+    def test_post_notification_fails_as_unauthenticated(
+        self, client: APIClient
+    ) -> None:
+        url: str = self.url()
+        response: Response = client.get(url)
+        assert response.status_code == 401
+
+    def test_post_notification_fails_with_user_unverified(
+        self, client: APIClient
+    ) -> None:
+        user: User = UserFaker()
+        url: str = self.url()
+        client.force_authenticate(user=user)
+        response: Response = client.get(url)
+        assert response.status_code == 403
+
+    def test_post_notification_fails_with_verified_user(
+        self, client: APIClient
+    ) -> None:
+        user: User = VerifiedUserFaker()
+        url: str = self.url()
+        client.force_authenticate(user=user)
+        response: Response = client.get(url)
+        assert response.status_code == 403
+
+    def test_post_notification_works_with_admin(
+        self, client: APIClient
+    ) -> None:
+        user: User = AdminFaker()
+        notification: Notification = NotificationTestFaker()
+        url: str = self.url()
+        client.force_authenticate(user=user)
+        response: Response = client.get(url)
+        assert response.status_code == 200
+        assert notification.id == response.data["results"][0]["id"]
+
+
+@pytest.mark.django_db
+class TestRetrieveNotificationsView:
+    def url(self, pk: int) -> str:
+        return reverse("emails:notifications-detail", args=[pk])
+
+    def test_url(self) -> None:
+        assert self.url(1) == "/api/notifications/1/"
+
+    def setup_method(self) -> None:
+        Email.objects.all().delete()
+        Block.objects.all().delete()
+        Notification.objects.all().delete()
+
+    def test_post_notification_fails_as_unauthenticated(
+        self, client: APIClient
+    ) -> None:
+        notification: Notification = NotificationTestFaker()
+        url: str = self.url(notification.id)
+        response: Response = client.get(url)
+        assert response.status_code == 401
+
+    def test_post_notification_fails_with_user_unverified(
+        self, client: APIClient
+    ) -> None:
+        notification: Notification = NotificationTestFaker()
+        user: User = UserFaker()
+        url: str = self.url(notification.id)
+        client.force_authenticate(user=user)
+        response: Response = client.get(url)
+        assert response.status_code == 403
+
+    def test_post_notification_fails_with_verified_user(
+        self, client: APIClient
+    ) -> None:
+        notification: Notification = NotificationTestFaker()
+        user: User = VerifiedUserFaker()
+        url: str = self.url(notification.id)
+        client.force_authenticate(user=user)
+        response: Response = client.get(url)
+        assert response.status_code == 403
+
+    def test_post_notification_works_with_admin(
+        self, client: APIClient
+    ) -> None:
+        user: User = AdminFaker()
+        notification: Notification = NotificationTestFaker()
+        url: str = self.url(notification.id)
+        client.force_authenticate(user=user)
+        response: Response = client.get(url)
+        assert response.status_code == 200
+        assert notification.id == response.data["id"]
+
+
+@pytest.mark.django_db
+class TestUpdateNotificationsView:
+    def url(self, pk: int) -> str:
+        return reverse("emails:notifications-detail", args=[pk])
+
+    def test_url(self) -> None:
+        assert self.url(1) == "/api/notifications/1/"
+
+    def setup_method(self) -> None:
+        Email.objects.all().delete()
+        Block.objects.all().delete()
+        Notification.objects.all().delete()
+
+    def test_post_notification_fails_as_unauthenticated(
+        self, client: APIClient
+    ) -> None:
+        notification: Notification = NotificationTestFaker()
+        data: dict = {
+            "header": "Email header",
+            "affair": "NOTIFICATION",
+            "subject": "Email subject",
+            "is_test": True,
+            "programed_send_date": "2050-12-12T10:59.000",
+            "blocks": [
+                {
+                    "title": "Block title",
+                    "content": "Block content",
+                    "link_text": "Click here",
+                    "show_link": True,
+                    "link": "google.com",
+                }
+            ],
+        }
+        url: str = self.url(notification.id)
+        response: Response = client.put(url, data=data, format="json")
+        assert response.status_code == 401
+
+    def test_post_notification_fails_with_user_unverified(
+        self, client: APIClient
+    ) -> None:
+        notification: Notification = NotificationTestFaker()
+        user: User = UserFaker()
+        data: dict = {
+            "header": "Email header",
+            "affair": "NOTIFICATION",
+            "subject": "Email subject",
+            "is_test": True,
+            "programed_send_date": "2050-12-12T10:59.000",
+            "blocks": [
+                {
+                    "title": "Block title",
+                    "content": "Block content",
+                    "link_text": "Click here",
+                    "show_link": True,
+                    "link": "google.com",
+                }
+            ],
+        }
+        url: str = self.url(notification.id)
+        client.force_authenticate(user=user)
+        response: Response = client.put(url, data=data, format="json")
+        assert response.status_code == 403
+
+    def test_post_notification_fails_with_verified_user(
+        self, client: APIClient
+    ) -> None:
+        notification: Notification = NotificationTestFaker()
+        user: User = VerifiedUserFaker()
+        client.force_authenticate(user=user)
+        data: dict = {
+            "header": "Email header",
+            "affair": "NOTIFICATION",
+            "subject": "Email subject",
+            "is_test": True,
+            "programed_send_date": "2050-12-12T10:59.000",
+            "blocks": [
+                {
+                    "title": "Block title",
+                    "content": "Block content",
+                    "link_text": "Click here",
+                    "show_link": True,
+                    "link": "google.com",
+                }
+            ],
+        }
+        url: str = self.url(notification.id)
+        response: Response = client.put(url, data=data, format="json")
+        assert response.status_code == 403
+
+    def test_post_notification_works_with_admin(
+        self, client: APIClient
+    ) -> None:
+        user: User = AdminFaker()
+        notification: Notification = NotificationTestFaker()
+        data: dict = {
+            "header": "Email header",
+            "affair": "NOTIFICATION",
+            "subject": "Email subject",
+            "is_test": True,
+            "programed_send_date": "2050-12-12T10:59.000",
+            "blocks": [
+                {
+                    "title": "Block title",
+                    "content": "Block content",
+                    "link_text": "Click here",
+                    "show_link": True,
+                    "link": "google.com",
+                }
+            ],
+        }
+        url: str = self.url(notification.id)
+        client.force_authenticate(user=user)
+        response: Response = client.put(url, data=data, format="json")
+        assert response.status_code == 200
+        assert notification.id == response.data["id"]
