c051fb8063d96031c857ed71cb5d38d54be960f6
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-09-20 00:39:19 +0200

    Add user/social_auth integration

--- Apps/Users/choices.py
+++ Apps/Users/choices.py
@@ -1,15 +1,22 @@
-from django.db import models
+from django.db.models import TextChoices
 
 
-class GenderChoices(models.TextChoices):
+class GenderChoices(TextChoices):
     FEMALE: tuple = ("F", "Female")
     MALE: tuple = ("M", "Male")
     NON_BINARY: tuple = ("N", "Non-binary")
     NOT_SAID: tuple = ("P", "Prefer not to say")
 
 
-class PreferredLanguageChoices(models.TextChoices):
+class PreferredLanguageChoices(TextChoices):
     ENGLISH: tuple = ("EN", "English")
     SPANISH: tuple = ("ES", "Spanish")
     FRENCH: tuple = ("FR", "French")
     OTHER: tuple = ("OT", "Other")
+
+
+class AuthProviders(TextChoices):
+    EMAIL: tuple = ("email", "Email")
+    FACEBOOK: tuple = ("facebook", "Facebook")
+    GOOGLE: tuple = ("google", "Google")
+    Twitter: tuple = ("twitter", "Twitter")
