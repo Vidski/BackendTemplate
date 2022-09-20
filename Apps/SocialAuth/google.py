2df4e5739570c29dc819634eb8ead97601412dcb
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-09-20 03:00:16 +0200

    Remove unnecesary file

--- Apps/SocialAuth/google.py
+++
@@ -1,9 +0,0 @@
-from google.auth.transport.requests import Request
-from google.oauth2.id_token import verify_oauth2_token
-
-
-class Google:
-    @staticmethod
-    def validate(auth_token: str) -> dict:
-        user_info: dict = verify_oauth2_token(auth_token, Request())
-        return user_info
