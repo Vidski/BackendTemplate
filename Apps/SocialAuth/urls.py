bd4356a69599a51f5631c329635dca3f7ada909f
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-09-25 01:23:50 +0200

    Refactor oauth urls

--- Apps/SocialAuth/urls.py
+++ Apps/SocialAuth/urls.py
@@ -1,6 +1,4 @@
-from django.urls import include
 from django.urls import path
-from rest_framework.routers import DefaultRouter
 
 from SocialAuth.views import FacebookSocialAuthView
 from SocialAuth.views import GoogleSocialAuthView
@@ -10,12 +8,5 @@ from SocialAuth.views import TwitterSocialAuthView
 urlpatterns = [
     path("google/", GoogleSocialAuthView.as_view()),
     path("facebook/", FacebookSocialAuthView.as_view()),
-    path("twitter/", FacebookSocialAuthView.as_view()),
+    path("twitter/", TwitterSocialAuthView.as_view()),
 ]
-
-# router: DefaultRouter = DefaultRouter()
-# router.register("google", GoogleSocialAuthView, basename="google")
-
-# urlpatterns: list = [
-#     path("google", include(router.urls)),
-# ]
