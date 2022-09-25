0adbe6a46d1a1245fc64507d13f02e17ba675a61
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2022-09-25 02:03:41 +0200

    Add tests to OAuth endpoints

---
+++ Apps/SocialAuth/tests/test_google_view.py
@@ -0,0 +1,55 @@
+import pytest
+from django.conf import settings
+from django.urls import reverse
+from mock import patch
+from mock.mock import MagicMock
+from rest_framework.response import Response
+from rest_framework.test import APIClient
+
+from Users.fakers.user import UserFaker
+from Users.models import User
+
+
+@pytest.fixture(scope="function")
+def client() -> APIClient:
+    return APIClient()
+
+
+@pytest.mark.django_db
+class TestGoogleAuthView:
+    def url(self) -> str:
+        return reverse("auth:google")
+
+    def test_url(self) -> None:
+        assert self.url() == "/api/oauth/google/"
+
+    @patch("SocialAuth.serializers.verify_oauth2_token")
+    def test_google_view_creates_new_user(
+        self, mock_verify_oauth2_token: MagicMock, client: APIClient
+    ) -> None:
+        email: str = "test@test.com"
+        mock_verify_oauth2_token.return_value = {
+            "email": email,
+            "given_name": "Test",
+            "family_name": "Test",
+            "aud": settings.GOOGLE_CLIENT_ID,
+        }
+        assert not User.objects.filter(email=email).exists()
+        response: Response = client.post(self.url(), {"token": "token"})
+        assert response.status_code == 200
+        assert User.objects.filter(email=email).exists()
+
+    @patch("SocialAuth.serializers.verify_oauth2_token")
+    def test_google_view_returns_user_login_data(
+        self, mock_verify_oauth2_token: MagicMock, client: APIClient
+    ) -> None:
+        email: str = "test@test.com"
+        UserFaker(email=email)
+        mock_verify_oauth2_token.return_value = {
+            "email": email,
+            "given_name": "Test",
+            "family_name": "Test",
+            "aud": settings.GOOGLE_CLIENT_ID,
+        }
+        response: Response = client.post(self.url(), {"token": "token"})
+        assert response.status_code == 200
